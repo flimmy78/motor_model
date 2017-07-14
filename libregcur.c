f6299711c08de48a97658c71303d58d76953d5ba
smolvik <johnwho454@gmail.com>
2017-07-14 19:40:32 +0300

    try add anti wind-up

--- libregcur.c
+++ libregcur.c
@@ -10,7 +10,7 @@
 #define r_IN(n, i) ((GetRealInPortPtrs(blk, n+1))[(i)]) 
 #define r_OUT(n, i) ((GetRealOutPortPtrs(blk, n+1))[(i)])
 
-//#define USE_SVPWM
+#define USE_SVPWM
 
 extern const int32_t cos_tb[1024];
 
@@ -34,10 +34,20 @@ inline int32_t mysin(int32_t a)
 	return cos_tb[1023&(a+3*MY_PI/2)];
 }
 
-inline void update(struct pi_reg_state *s, int32_t e)
+inline void update(scicos_block *blk, struct pi_reg_state *s, int32_t e, int32_t fs)
 {
-    s->a += s->ki*e;
-	s->y = e*s->kp + s->a;	
+	int32_t a = s->a;
+	int32_t d = s->ki*e;
+	
+	// will accumulator grow up?
+	if(fs) if( ((a>0)&&(d>0))||((a<0)&&(d<0)) ) d = 0;
+
+	a += d;
+	s->y = e*s->kp + a;
+	s->a = a;
+	
+	r_OUT(1, 0) = e;
+	r_OUT(1, 1) = 0;	
 
 	//s->y = 1024*e + s->a;
 	//s->a = s->y - 782*e;
@@ -68,6 +78,7 @@ inline void dq_to_abc(int32_t *abc, int32_t *dq, int32_t angle)
 	abc[2] = (dq[0]*cos_tb[1023&(angle+2*512/3)] - dq[1]*cos_tb[1023&(angle+2*512/3+3*512/2)]) >> 20;
 }
 
+// calc magnitude and angle of vector using CORDIC algorithm
 inline void cord_atan(int32_t *v, int32_t *ang, int32_t *mag)
 {
 	const int32_t AngTable[] = {128, 76, 40, 20, 10, 5, 3, 1};
@@ -103,71 +114,91 @@ inline void cord_atan(int32_t *v, int32_t *ang, int32_t *mag)
 	if(SumAngle < 0) SumAngle += 2*MY_PI;	
 	
 	*ang = SumAngle;
-	*mag = (kc[ns-1]*x)/1024;
+	*mag = (kc[ns-1]*x) >> 10;
 }
 
-inline void svpwm(int32_t *abc, int32_t *dq, int32_t phase)
+inline int32_t svpwm(int32_t *abc, int32_t *dq, int32_t phase)
 {
+	int32_t fs = 0;
 	int32_t mag;
 	int32_t ang;
 	cord_atan(dq, &ang, &mag);
 	
-	mag /= 1024;
+	mag = mag >> 10;
 	int32_t phi = 1023&(phase + ang);
-
-	if(phi<MY_PI/3){
-		int32_t r1 = mag*mysin(7*MY_PI/3-phi) >> 10;
-		int32_t r2 = mag*mysin(phi) >> 10;
+	
+	if(mag > 720) {
+		mag = 720;
+		fs = 1;
+	}
+	else fs = 0;
+			
+	int32_t ns = (phi*6) >> 10;	 // get the sector number
+	int32_t r1;
+	int32_t r2;
+	
+	switch(ns){
+		case 0:
+		r1 = mag*mysin(7*MY_PI/3-phi) >> 10;
+		r2 = mag*mysin(phi) >> 10;
 		
 		abc[0] = r1+r2;
 		abc[1] = -r1+r2;
-		abc[2] = -r1-r2;
-	}
-	else if(phi<2*MY_PI/3){
+		abc[2] = -r1-r2;			
+		break;
+		
+		case 1:
 		phi -= MY_PI/3;
-		int32_t r1 = mag*mysin(7*MY_PI/3-phi) >> 10;
-		int32_t r2 = mag*mysin(phi) >> 10;
+		r1 = mag*mysin(7*MY_PI/3-phi) >> 10;
+		r2 = mag*mysin(phi) >> 10;
 		
 		abc[0] = r1-r2;
 		abc[1] = r1+r2;
 		abc[2] = -r1-r2;		
-	}
-	else if(phi<MY_PI){
+		break;
+		
+		case 2:
 		phi -= 2*MY_PI/3;
-		int32_t r1 = mag*mysin(7*MY_PI/3-phi) >> 10;
-		int32_t r2 = mag*mysin(phi) >> 10;
+		r1 = mag*mysin(7*MY_PI/3-phi) >> 10;
+		r2 = mag*mysin(phi) >> 10;
 		
 		abc[0] = -r1-r2;
 		abc[1] = r1+r2;
 		abc[2] = -r1+r2;
-	}
-	else if(phi<4*MY_PI/3){
+		break;
+		
+		case 3:
 		phi -= 3*MY_PI/3;
-		int32_t r1 = mag*mysin(7*MY_PI/3-phi) >> 10;
-		int32_t r2 = mag*mysin(phi) >> 10;
+		r1 = mag*mysin(7*MY_PI/3-phi) >> 10;
+		r2 = mag*mysin(phi) >> 10;
 		
 		abc[0] = -r1-r2;
 		abc[1] = r1-r2;
 		abc[2] = r1+r2;		
-	}		
-	else if(phi<5*MY_PI/3){
+		break;
+		
+		case 4:
 		phi -= 4*MY_PI/3;
-		int32_t r1 = mag*mysin(7*MY_PI/3-phi) >> 10;
-		int32_t r2 = mag*mysin(phi) >> 10;
+		r1 = mag*mysin(7*MY_PI/3-phi) >> 10;
+		r2 = mag*mysin(phi) >> 10;
 		
 		abc[0] = -r1+r2;
 		abc[1] = -r1-r2;
 		abc[2] = r1+r2;	
-	}			
-	else if(phi<2*MY_PI){
+		break;
+		
+		case 5:
 		phi -= 5*MY_PI/3;
-		int32_t r1 = mag*mysin(7*MY_PI/3-phi) >> 10;
-		int32_t r2 = mag*mysin(phi) >> 10;
+		r1 = mag*mysin(7*MY_PI/3-phi) >> 10;
+		r2 = mag*mysin(phi) >> 10;
 		
 		abc[0] = +r1+r2;
 		abc[1] = -r1-r2;
 		abc[2] = r1-r2;
-	}	
+		break;
+	}
+	
+	return fs;
 }
 
 void pi_reg_cur(scicos_block *blk, int flag)
@@ -175,6 +206,7 @@ void pi_reg_cur(scicos_block *blk, int flag)
 	static struct pi_reg_state dreg = {0, 0, 0, 0};
 	static struct pi_reg_state qreg = {0, 0, 0, 0};
 	static int32_t phase = 0;
+	static int32_t fsat = 0;
 	
 	int32_t dq[2];	
 	int32_t abc[3];
@@ -186,7 +218,7 @@ void pi_reg_cur(scicos_block *blk, int flag)
     {
 	case 1:		
 		// convert dq voltages to abc			
-				
+
 #ifdef USE_SVPWM
 
 		dq[0] = dreg.y;
@@ -195,7 +227,7 @@ void pi_reg_cur(scicos_block *blk, int flag)
 		r_OUT(1, 0) = 0;
 		r_OUT(1, 1) = 0;
 		
-		svpwm(abc, dq, phase);
+		fsat = svpwm(abc, dq, phase);
 		r_OUT(0, 0) = (double)abc[0];
 		r_OUT(0, 1) = (double)abc[1];
 		r_OUT(0, 2) = (double)abc[2];
@@ -208,17 +240,17 @@ void pi_reg_cur(scicos_block *blk, int flag)
 		r_OUT(0, 1) = (double)abc[1];
 		r_OUT(0, 2) = (double)abc[2];		
 #endif							    
-							    
+
 	break;
 	case 2:		
 		
-		dreg.ki = r_IN(2, 0)*1024;
-		dreg.kp = r_IN(3, 0)*1024;
+		dreg.ki = (int32_t)(r_IN(2, 0)*1024);
+		dreg.kp = (int32_t)(r_IN(3, 0)*1024);
 		
-		qreg.ki = r_IN(2, 0)*1024;
-		qreg.kp = r_IN(3, 0)*1024;
+		qreg.ki = (int32_t)(r_IN(2, 0)*1024);
+		qreg.kp = (int32_t)(r_IN(3, 0)*1024);
 
-		// get the motor electrical angle
+		// get the motor electrical angle x4 mechanical angle
 		phase = ( (uint32_t)r_IN(4, 0) ) & (1024-1);
 
 		// get the currents from ADC
@@ -233,8 +265,8 @@ void pi_reg_cur(scicos_block *blk, int flag)
 		eq = (int32_t)r_IN(1, 1) - dq[1];
 		
 		// regulators do its work
-		update( &dreg, ed );
-		update( &qreg, eq );
+		update(blk, &dreg, ed , fsat);
+		update(blk, &qreg, eq , fsat);
 
 	break;
 	case 4:
